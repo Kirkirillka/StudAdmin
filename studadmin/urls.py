8f966e1510773f430971c8ddbe7e0efba7153bbb
kirill <kirillkukarcev@gmail.com>
2016-09-01 17:59:00 +0300

    Url patterns is rebuilded.Also 2 buttons on AddPromotion pannel is commented

--- studadmin/urls.py
+++ studadmin/urls.py
@@ -1,38 +1,45 @@
 __author__ = 'kirill'
 
-
-from django.conf.urls import url
-from .views import AddStudentView,\
-    StudentDetailView,\
-    AddPromotionView,\
-    StudentListView,\
-    index,\
-    AddViolationView,\
-    ChooseViolationView,\
-    Login,\
-    Logout,\
-    AddViolationToStudentView,\
-    AddPromotionToStudentView,\
-    ViolationDetail,\
+from django.conf.urls import url, include
+from .views import AddStudentView, \
+    StudentDetailView, \
+    AddPromotionView, \
+    StudentListView, \
+    index, \
+    AddViolationView, \
+    ChooseViolationView, \
+    Login, \
+    Logout, \
+    AddViolationToStudentView, \
+    AddPromotionToStudentView, \
+    ViolationDetail, \
     PromotionDetail
 
+app_name = 'studadmin'
 
 
 
-app_name='studadmin'
+violation_patterns = [url(r'^add_violetion/$', AddViolationView.as_view(), name='add_violation'),
+                      url(r'add_violetion_to_student/(?P<student_id>[0-9]+)/$', AddViolationToStudentView.as_view(),
+                          name='add_violation_to_student'),
+                      url(r'^(?P<pk>[0-9]+)/$', ViolationDetail.as_view(), name='violation_detail'),
+                      url(r'^choose_violation/$', ChooseViolationView.as_view(), name='choose_violation'),
+                      ]
 
-urlpatterns=[
-    url(r'^$',index.as_view(),name='index'),
-    url(r'AddStudent/$',AddStudentView.as_view(),name='add_student'),
-    url(r'^Students/?page=(?P<page>[0-9]+)/$',StudentListView.as_view(),name='students_list'),
-    url(r'^Student/(?P<pk>[0-9]+)/$',StudentDetailView.as_view(),name='student_detail'),
-    url(r'^Violation/(?P<pk>[0-9]+)/$',ViolationDetail.as_view(),name='violation_detail'),
-    url(r'^AddPromotion/$',AddPromotionView.as_view(),name='add_promotion'),
-    url(r'^Promotion/(?P<pk>[0-9]+)/$',PromotionDetail.as_view(),name='promotion_detail'),
-    url(r'^AddVioletion/$',AddViolationView.as_view(),name='add_violation'),
-    url(r'AddVioletionToStudent/(?P<student_id>[0-9]+)/$',AddViolationToStudentView.as_view(),name='add_violation_to_student'),
-    url(r'AddPromotionToStudent/(?P<student_id>[0-9]+)/$',AddPromotionToStudentView.as_view(),name='add_promotion_to_student'),
-    url(r'^ChooseViolation/$',ChooseViolationView.as_view(),name='choose_violation'),
-    url(r'^Login/$',Login.as_view(),name='login'),
-    url(r'^Logout/$',Logout.as_view(),name='logout')
+promotion_patterns = [url(r'^add_promotion/$', AddPromotionView.as_view(), name='add_promotion'),
+                      url(r'^(?P<pk>[0-9]+)/$', PromotionDetail.as_view(), name='promotion_detail'),
+                      url(r'add_promotion_to_student/(?P<student_id>[0-9]+)/$', AddPromotionToStudentView.as_view(),
+                          name='add_promotion_to_student'), ]
+
+
+
+urlpatterns = [
+    url(r'^$', index.as_view(), name='index'),
+    url(r'add_student/$', AddStudentView.as_view(), name='add_student'),
+    url(r'^students_list//?page=(?P<page>[0-9]+)/$', StudentListView.as_view(), name='students_list'),
+    url(r'^student_detail/(?P<pk>[0-9]+)/$', StudentDetailView.as_view(), name='student_detail'),
+    url(r'violation/',include(violation_patterns)),
+    url(r'promotion/',include(promotion_patterns)),
+    url(r'^Login/$', Login.as_view(), name='login'),
+    url(r'^Logout/$', Logout.as_view(), name='logout')
 ]
